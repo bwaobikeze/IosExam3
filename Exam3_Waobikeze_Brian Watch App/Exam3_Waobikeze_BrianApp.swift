b978c30068c82840c0bbaad0d572e0c756f33051
bwaobikeze <bwaobikeze@my.lonestar.edu>
2023-11-16 15:35:04 -0600

    Initial Commit

---
+++ Exam3_Waobikeze_Brian Watch App/Exam3_Waobikeze_BrianApp.swift
@@ -0,0 +1,17 @@
+//
+//  Exam3_Waobikeze_BrianApp.swift
+//  Exam3_Waobikeze_Brian Watch App
+//
+//  Created by brian waobikeze on 11/16/23.
+//
+
+import SwiftUI
+
+@main
+struct Exam3_Waobikeze_Brian_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Exam3_Waobikeze_Brian/Exam3_Waobikeze_BrianApp.swift
@@ -0,0 +1,17 @@
+//
+//  Exam3_Waobikeze_BrianApp.swift
+//  Exam3_Waobikeze_Brian
+//
+//  Created by brian waobikeze on 11/16/23.
+//
+
+import SwiftUI
+
+@main
+struct Exam3_Waobikeze_BrianApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
